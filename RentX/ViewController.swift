6ec7eb6a61c578b3006e66a6ec81a028feaa06dd
Rafael V. dos Santos <rvsantos00@gmail.com>
2021-02-09 01:42:36 -0300

    Initial Commit

---
+++ RentX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RentX
+//
+//  Created by Rafael V. dos Santos on 09/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
