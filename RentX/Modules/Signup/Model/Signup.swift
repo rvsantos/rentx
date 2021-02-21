b55775022a4be0bd229297a0ad8d6ec224c651fe
Rafael V. dos Santos <rvsantos00@gmail.com>
2021-02-21 16:37:08 -0300

    Removing CollectionView of SignupController

---
+++ RentX/Modules/Signup/Model/Signup.swift
@@ -0,0 +1,14 @@
+//
+//  Signup.swift
+//  RentX
+//
+//  Created by Rafael V. dos Santos on 20/02/21.
+//
+
+import Foundation
+
+struct Signup: Codable {
+    let name: String
+    let email: String
+    let password: String
+}
---
+++ RentX/Modules/Signup/ViewModel/SignupVM.swift
@@ -0,0 +1,15 @@
+//
+//  SignupVM.swift
+//  RentX
+//
+//  Created by Rafael V. dos Santos on 20/02/21.
+//
+
+import Foundation
+
+struct SignupVM {
+    var name: String?
+    var email: String?
+    var password: String?
+    var repeatPassword: String?
+}
